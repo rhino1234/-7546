41879f4aca651cc97fb8df065eb6bb9dcab1b10f
rhino1234 <epa090304@gmail.com>
2022-02-07 19:15:30 +0000

    start

---
+++ pass.js
@@ -0,0 +1,12 @@
+
+    //password code
+var response = prompt("password?");
+var password = "Cat3456"
+if (response === password) {
+  alert("Welcome!"); 
+  //window.location.assign("/8786479.html"); 
+}
+else {
+  alert("Go Away!");
+  window.location.assign("/denied.html"); 
+}
